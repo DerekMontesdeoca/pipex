42af66b156d1ad29308f4ea19f7b6d97dbd8d371
Derek Montesdeoca <demont93@gmail.com>
2025-03-28 19:07:12 +0100

    refactor: start extracting function out of main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dmontesd <dmontesd@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 19:08:28 by dmontesd          #+#    #+#             */
-/*   Updated: 2025/03/28 05:00:55 by dmontesd         ###   ########.fr       */
+/*   Updated: 2025/03/28 11:38:35 by dmontesd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
@@ -16,101 +16,11 @@
 #include <wait.h>
 #include <stdio.h>
 #include "libft.h"
-
-typedef struct s_env_path
-{
-	char	**paths;
-	int		size;
-}	t_env_path;
-
-static int count_paths(const char *str)
-{
-	const char	*begin;
-	const char	*end;
-	int			count;
-
-	begin = str;
-	end = str;
-	count = 0;
-	while (*end != '\0')
-	{
-		if (*end == ':')
-		{
-			if (end != begin)
-				++count;
-			begin = end + 1;
-		}
-		++end;
-	}
-	if (begin != end)
-		++count;
-	return (count);
-}
-
-static int	find_path_variable(char **envp)
-{
-	size_t	i;
-
-	i = 0;
-	while (envp[i] != NULL)
-	{
-		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
-			return (i);
-		++i;
-	}
-	return (-1);
-}
-
-void	env_path_split(t_env_path *env_path, char *raw_path)
-{
-	char	*begin;
-	char	*end;
-	size_t	i;
-	
-	begin = raw_path;
-	end = raw_path;
-	while (*end != '\0')
-	{
-		if (*end == ':')
-		{
-			*end = '\0';
-			if (end != begin)
-				env_path->paths[i++] = begin;
-			begin = end + 1;
-		}
-		++end;
-	}
-	if (begin != end)
-		env_path->paths[i++] = begin;
-}
-
-int	env_path_make(t_env_path *env_path, char **envp)
-{
-	int		path_index;
-	char	*raw_path;
-
-	env_path->paths = NULL;
-	env_path->size = 0;
-	path_index = find_path_variable(envp);
-	if (path_index < 0)
-		return (0);
-	raw_path = envp[path_index] + 5;
-	if (raw_path[0] == '\0')
-		return (0);
-	env_path->size = count_paths(raw_path);
-	env_path->paths = malloc(env_path->size);
-	if (env_path->paths == NULL)
-		return (-1);
-	env_path_split(env_path, raw_path);
-	return (0);
-}
+#include "pipex.h"
 
 // Todo: Im thinking of allocating a single string and using it to try all
 // paths. Its a good idea. I need to calculate the length of the max path. It 
 // can be pre-cached at read time. Maybe I need to expand my path struct.
-// path_join()
-
-
 int	ft_execvpe(
 		t_env_path *env_path, char *path, char **const argv, char **const envp
 ) {
@@ -124,7 +34,6 @@ int	ft_execvpe(
 		execve(final_path, argv, envp);
 	}
 }
-
 
 int	pipe_child_setup(int *pip_out, int *pip_in)
 {
--- pipex.h
+++ pipex.h
@@ -6,7 +6,22 @@
 /*   By: dmontesd <dmontesd@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 19:06:51 by dmontesd          #+#    #+#             */
-/*   Updated: 2025/03/27 19:06:52 by dmontesd         ###   ########.fr       */
+/*   Updated: 2025/03/28 18:15:30 by dmontesd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef PIPEX_H
+# define PIPEX_H
 
+# include <stddef.h>
+
+typedef struct s_env_path
+{
+	char	**paths;
+	int		size;
+}	t_env_path;
+
+int	env_path_make(t_env_path *env_path, char **envp);
+
+void path_join(const char *head, const char *tail, char *result, size_t size);
+
+#endif
